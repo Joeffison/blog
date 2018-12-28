95950211c8cf576f24a08932ac4255bf3cfcd31a
Joeffison Silverio de Andrade <joeffison@gmail.com>
2018-12-28 23:54:14 +0100

    Setting Bootstrap

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
