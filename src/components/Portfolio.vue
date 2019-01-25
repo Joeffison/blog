121e0541c86a9cedd79d1281b7fc4701ab963997
Joeffison Silverio de Andrade <joeffison@gmail.com>
2019-01-25 19:19:42 +0100

    Fixing active tab styling (Portfolio Component)

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -3,37 +3,14 @@
     <page-separator-tooltip></page-separator-tooltip>
     <article class="app-content-container">
       <b-card no-body>
-        <b-tabs pills cards vertical>
-          <b-tab :title="$t('This website')"
-                 title-item-class="portfolio-li rounded"
-                 title-link-class="portfolio-tab-item rounded"
-                 active
+        <b-tabs pills cards vertical v-model="tabIndex">
+          <b-tab
+            v-for="(tab, index) in tabs" :key="index"
+            :title="$t(tab.title)"
+            :title-item-class="'portfolio-li rounded' + getActiveTabClass(index)"
+            :title-link-class="'portfolio-tab-item rounded' + getActiveTabClass(index)"
+            v-html="$t(tab.content)"
           >
-            {{ $t('#portfolio-this-website') }}
-          </b-tab>
-          <b-tab :title="$t('Coding Challenges')"
-                 title-item-class="portfolio-li rounded"
-                 title-link-class="portfolio-tab-item"
-          >
-            <span v-html="$t('#portfolio-coding-challenges')"></span>
-          </b-tab>
-          <b-tab :title="$t('Machine Learning Builder')"
-                 title-item-class="portfolio-li rounded"
-                 title-link-class="portfolio-tab-item"
-          >
-            {{ $t('#portfolio-ml-builder') }}
-          </b-tab>
-          <b-tab :title="$t('Instagram Utility')"
-                 title-item-class="portfolio-li rounded"
-                 title-link-class="portfolio-tab-item"
-          >
-            {{ $t('#portfolio-instagram-utility') }}
-          </b-tab>
-          <b-tab :title="$t('Academic Research')"
-                 title-item-class="portfolio-li rounded"
-                 title-link-class="portfolio-tab-item"
-         >
-            {{ $t('#portfolio-academic-research') }}
           </b-tab>
         </b-tabs>
       </b-card>
@@ -46,7 +23,24 @@ import PageSeparatorTooltip from '@/components/PageSeparatorTooltip'
 
 export default {
   name: 'AppPortfolio',
-  components: { PageSeparatorTooltip }
+  components: { PageSeparatorTooltip },
+  data () {
+    return {
+      tabIndex: 0,
+      tabs: [
+        {title: 'This website', content: '#portfolio-this-website'},
+        {title: 'Coding Challenges', content: '#portfolio-coding-challenges'},
+        {title: 'Machine Learning Builder', content: '#portfolio-ml-builder'},
+        {title: 'Instagram Utility', content: '#portfolio-instagram-utility'},
+        {title: 'Academic Research', content: '#portfolio-academic-research'}
+      ]
+    }
+  },
+  methods: {
+    getActiveTabClass (index) {
+      return index === this.tabIndex ? ' active' : ''
+    }
+  }
 }
 </script>
 
