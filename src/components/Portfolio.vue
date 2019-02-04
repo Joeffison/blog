d47b32a4fe07cf43f168273ef334f5672b0d708f
Joeffison Silverio de Andrade <joeffison@gmail.com>
2019-02-04 08:42:40 +0100

    Portfolio Tabs: Vertically Centering, Fixing Horizontal Centering and Fixing responsiveness behaviour.

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -3,12 +3,15 @@
     <page-separator-tooltip></page-separator-tooltip>
     <article class="app-content-container">
       <b-card no-body>
-        <b-tabs pills cards vertical v-model="tabIndex">
+        <b-tabs pills cards vertical v-model="tabIndex"
+                class="portfolio-tab-component"
+                nav-wrapper-class="portfolio-tab-nav"
+                content-class="portfolio-tab-content">
           <b-tab
             v-for="(tab, index) in tabs" :key="index"
             :title="$t(tab.title)"
-            :title-item-class="'portfolio-li rounded' + getActiveTabClass(index)"
-            :title-link-class="'portfolio-tab-item rounded' + getActiveTabClass(index)"
+            :title-item-class="'portfolio-tab-nav-li rounded' + getActiveTabClass(index)"
+            :title-link-class="'portfolio-tab-nav-li-content rounded' + getActiveTabClass(index)"
             v-html="$t(tab.content)"
           >
           </b-tab>
